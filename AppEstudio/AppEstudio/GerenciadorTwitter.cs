44b15fbbec92567b89f5190f3b2d79d9636f8b78
brianathayde <brian_athayde@hotmail.com>
2017-03-07 06:32:01 -0300

    Add files via upload

---
+++ AppEstudio/AppEstudio/GerenciadorTwitter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Threading;
+
+
+using Tweetinvi;
+
+namespace AppEstudio
+{
+    class GerenciadorTwitter
+    {
+
+        string resultado = "";
+        Thread t;
+
+        public static string TAG = "#bbb";
+
+        public GerenciadorTwitter()
+        {
+            //Tweetinvi Tutorial - Introduction to Twitter in C#
+            //www.youtube.com/watch?v=1maeTudF8cQ
+            //Encontre em: apps.twitter.com
+            Auth.SetUserCredentials("Y35l7y7Z6HN1E8viG9LU2P4PB", "8pKZ3ttYe02vyKCQeJUSxfTorVIXGv9QIXAlI6oLikc5Bx0ake",
+                "2509227109-Sy3UQS2AfATSsdJevQxbWradahTRJuqWZp9pxQO", "Ab1yaNzSHEGUSDOHAar13aPi34Wwtn01JJcyFwWqvBEEs");          
+        }
+
+        void Procurar()
+        {
+            try
+            {
+                var tweets = Search.SearchTweets(TAG);
+                //***Nenhum tweet começa com espaço***
+                //***Usar espaço para diferenciar user/text***
+                var user = User.GetAuthenticatedUser();
+                Array a = tweets.ToArray();
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (!tweets.ElementAt(i).IsRetweet)
+                    {
+                        resultado = tweets.ElementAt(i).CreatedBy.ScreenName + " " + tweets.ElementAt(i).FullText;
+                        i = a.Length;
+                    }
+                }
+            }
+            catch
+            {
+            }
+            
+        }
+
+        public string Tweet()
+        {
+            Procurar();
+            return resultado;
+        }
+    }
+}
---
+++ AppEstudio/AppEstudio/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace AppEstudio
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+
+    public partial class MainWindow : Window
+    {
+
+
+        TcpClient client;
+        NetworkStream stream;
+        GerenciadorTwitter g;
+        Thread t;
+        
+
+        void Atualizar()
+        {
+            while (true)
+            {
+                Connect(g.Tweet());
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            g = new GerenciadorTwitter();
+
+            t = new Thread(Atualizar);
+            t.Start();
+            Thread.Sleep(20);
+        }
+
+
+        void Connect(string message)
+        {
+            string server = "127.0.0.1";
+            
+            if (client == null)
+            {
+                try
+                {
+                    Int32 port = 13000;
+                    client = new TcpClient(server, port);
+                    // Translate the passed message into ASCII and store it as a Byte array.
+                    Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+                    stream = client.GetStream();
+                    // Send the message to the connected TcpServer. 
+                    stream.Write(data, 0, data.Length);
+
+                }
+                catch
+                {
+                }
+            }
+            else
+            {
+                try
+                {
+                    Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+                    stream = client.GetStream();
+                    stream.Write(data, 0, data.Length);
+                }
+                catch
+                {
+                    client = null;
+                    try
+                    {
+                        Int32 port = 13000;
+                        client = new TcpClient(server, port);
+                        // Translate the passed message into ASCII and store it as a Byte array.
+                        Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+                        stream = client.GetStream();
+                        // Send the message to the connected TcpServer. 
+                        stream.Write(data, 0, data.Length);
+
+                    }
+                    catch
+                    {
+                    }
+                }
+
+            }
+        }
+   
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            t.Abort();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            GerenciadorTwitter.TAG = textBox.Text;
+        }
+    }
+
+}
